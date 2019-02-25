72bc16daae7825473631b8a892efe3d6d1aa806d
mizutanian <mizuta@basicinc.jp>
2019-02-25 19:23:07 +0900

    メールバリデーター

--- javascript/main.js
+++ javascript/main.js
@@ -18,3 +18,21 @@ function readHtml(url) {
       }
   });
 }
+
+function validateContent(){
+  content = document.getElementsByName("mail_content")[0].value;
+  arr = content.split(/\r\n|\r|\n/);
+  good = true;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf("http") != -1){
+      num_question = arr[i].split("?").length - 1;
+      if (num_question > 1){
+        good = false;
+        alert("1つのURLに?が2つ以上ありませんか？\n該当URL:"+arr[i]);
+      }
+    };
+  }
+  if(good == true){
+    alert("URLに?が正常に設置されています");
+  }
+};
