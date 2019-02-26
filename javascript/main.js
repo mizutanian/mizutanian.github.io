128259502dbf9e955072ae37e73cf482d157acbe
mizutanian <mizuta@basicinc.jp>
2019-02-26 13:03:48 +0900

    mailvalidater

--- javascript/main.js
+++ javascript/main.js
@@ -22,17 +22,17 @@ function readHtml(url) {
 function validateContent(){
   content = document.getElementsByName("mail_content")[0].value;
   arr = content.split(/\r\n|\r|\n/);
-  good = true;
+  var h = "<h2>Result</h2>";
   for (i = 0; i < arr.length; i++) {
     if (arr[i].indexOf("http") != -1){
       num_question = arr[i].split("?").length - 1;
       if (num_question > 1){
-        good = false;
-        alert("1つのURLに?が2つ以上ありませんか？\n該当URL:"+arr[i]);
+//        alert("1つのURLに?が2つ以上ありませんか？\n該当URL:"+arr[i]);
+        h = h + '<div class="incorrect">'+ arr[i] +'</div>';
+      }else{
+        h = h + '<div class="correct">'+ arr[i] +'</div>';
       }
     };
   }
-  if(good == true){
-    alert("URLに?が正常に設置されています");
-  }
+  document.getElementById("result").innerHTML = h ;
 };
